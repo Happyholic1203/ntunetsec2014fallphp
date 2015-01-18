826d96298f2ced178d82c988ac9c23b201f2cdf9
Meng-Han John Tsai <mhtsai1010@gmail.com>
2015-01-18 18:47:24 +0800

    test class object

--- web/db.php
+++ web/db.php
@@ -4,7 +4,7 @@ define("MONGODB_DATABASE", 'ntunetsec2014fall');
 define("MONGODB_USER_COLLECTION", 'User');
 define("MONGODB_RECORD_COLLECTION", 'Record');
 
-class DB {
+class Mongo {
     private $self = array();
 
     private $connection = NULL;
@@ -40,7 +40,6 @@ class DB {
     // Database initialization
     public function init() {
         // Connect to mongodb
-
         $this->connection = new MongoClient( $this->self['dbUrl'], [
             'username' => $this->self['dbUser'],
             'password' => $this->self['dbPass'],
@@ -49,30 +48,13 @@ class DB {
 
         // Choose database and collection
         if($this->connection) {
-/*
-            //print_r($this->connection->getConnections());
-
-            //$db = $m->selectDB(MONGODB_DATABASE);
-            $db = $mongo->MONGODB_DATABASE;
-            $collections = $db->listCollections();
-
-            foreach ($collections as $collection) {
-                echo "amount of documents in $collection: ";
-                echo $collection->count(), "\n";
-            }
-*/
-
             $this->database = $this->connection
                 ->selectDB(MONGODB_DATABASE);
-
             $this->userCollection = $this->database
                 ->selectCollection(MONGODB_USER_COLLECTION);
-
             $this->recordCollection = $this->database
                 ->selectCollection(MONGODB_RECORD_COLLECTION);
-
         }
-        //echo "connection has been setup!\n";
     }
 
     // Database termination
@@ -80,29 +62,29 @@ class DB {
         // Close mongodb connection
         if($this->connection) {
             $closed = $this->$connection->close();
-            //echo "connection has been terminated!\n";
         }
     }
 
-    public function dump() {
-        $cursor = $this->userCollection->find();
-        foreach ( $cursor as $id => $value )
-        {
-            echo "$id: ";
-            var_dump( $value );
-            // why? no results?
-        }
+    //
+    public function isUserEmailOccupied($email) {
+        $cursor = $this->userCollection->findOne(
+            array('email' => $email));
+        if ($cursor !== NULL)
+            return TRUE;
+        else
+            return FALSE;
     }
 
 }
 
-echo "new db object\n";
-$db= new DB();
-echo "connect to db\n";
+$db = new Mongo();
 $db->init();
-echo "dump all data\n";
-$db->dump();
-echo "close connection\n";
+$test = $db->isUserEmailOccupied("seller@example.com");
+if($test)
+    echo "Good job";
+else
+    echo "Damn";
 $db->close();
+
 
 ?>
