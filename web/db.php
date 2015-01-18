e7d9751d3df2cccdf3e2ac6ec07277b4de414118
Meng-Han John Tsai <mhtsai1010@gmail.com>
2015-01-18 16:05:57 +0800

    test class object

--- web/db.php
+++ web/db.php
@@ -1,27 +1,76 @@
 <?php
-echo "before conn";
 
-$connection = new MongoClient(getenv('MongoURL'),[
-    'username' => getenv('MongoUser'),
-    'password' => getenv('MongoPass'),
-    'db'       => getenv('MongoDB')
-]);
+class DB {
 
-echo "after conn";
-echo "check conn";
-if ($connection) {
-    echo "why1";
-    $collection = $connection->ntunetsec2014fall->User;
-    echo "why2";
-    $document = $collection->findOne();
-    echo "why3";
-    var_dump( $document );
-    echo "why4";
-    $closed = $connection->close();
+    private $self = array();
+
+    private $mongodb_database = 'ntunetsec2014fall';
+    private $mongodb_user_collection = 'User';
+    private $mongodb_record_collection = 'Record';
+
+    private $connection = NULL;
+    private $database = NULL;
+    private $userCollection = NULL;
+    private $recordCollection = NULL;
+
+    public function __construct($param) {
+        // Setup default variables
+        $this->self['dbUrl']  = getenv('MongoURL');
+        $this->self['dbUser'] = getenv('MongoUser');
+        $this->self['dbPass'] = getenv('MongoPass');
+        $this->self['dbName'] = getenv('MongoDB');
+
+        // User-defined variables
+        if (is_array($param)) {
+            if (isset($param['url'])) {
+                $this->self['dbUrl'] = $param['url'];
+            }
+            if (isset($param['user'])) {
+                $this->self['dbUser'] = $param['user'];
+            }
+            if (isset($param['pass'])) {
+                $this->self['dbPass'] = $param['pass'];
+            }
+            if (isset($param['database'])) {
+                $this->self['dbName'] = $param['database'];
+            }
+        }
+    }
+
+    public function init() {
+        // Connect to mongodb
+        $this->connection = new MongoClient( $this->self['dbUrl'], [
+            'username' => $this->self['dbUser'],
+            'password' => $this->self['dbPass'],
+            'db'       => $this->self['dbName'],
+            'ssl'      => true
+        ]);
+        // Choose database and collection
+        if($this->connection) {
+            $this->database = $this->connection->$mongodb_database;
+            $this->userCollection = $this->connection->
+                $mongodb_database->$mongodb_user_collection;
+            $this->recordCollection = $this->connection->
+                $mongodb_database->$mongodb_record_collection;
+        }
+        echo "connection has been setup!\n";
+    }
+
+    public function close() {
+        // Close mongodb connection
+        if($this->connection) {
+            $closed = $connection->close();
+            echo "connection has been terminated!\n";
+        }
+    }
+
 }
-else {
-    echo "damn";
-}
-echo "close conn";
+
+echo "new db object\n";
+$test = new DB();
+echo "connect to db\n";
+$test.init();
+echo "close connection\n";
+$test.close();
 
 ?>
