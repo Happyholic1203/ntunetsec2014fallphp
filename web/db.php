265764db85325d15945612e94835e5b8db92cbcc
Meng-Han John Tsai <mhtsai1010@gmail.com>
2015-01-18 14:09:55 +0800

    test db

---
+++ web/db.php
@@ -0,0 +1,17 @@
+<?php
+
+$connection = new MongoClient(getenv('MongoURL'),[
+    'username' => getenv('MongoUser'),
+    'password' => getenv('MongoPassword'),
+    'db'       => getenv('MongoDB')
+]);
+
+if ($connecion) {
+    $collection = $connection->database->collectionName;
+    $document = $collection->findOne();
+    var_dump( $document );
+}
+
+$closed = $connection->close();
+
+?>
