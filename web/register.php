c47010dd9ae87a9b9139f32f35f154b2369f9828
Meng-Han John Tsai <mhtsai1010@gmail.com>
2015-01-20 15:52:20 +0800

    register page testing

--- web/register.php
+++ web/register.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     }
 
                     // Validates user type
-                    $type = $_POST['email'] ?: 0;
+                    $type = $_POST['type'] ?: 0;
                     if ($type === 'buyer' || $type === 'seller') {
 
                         // User registration
