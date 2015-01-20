bd8ccfc84585b9bb8160523917a731f9ff7d9338
Meng-Han John Tsai <mhtsai1010@gmail.com>
2015-01-20 10:56:12 +0800

    register page testing

--- web/register.php
+++ web/register.php
@@ -1,4 +1,6 @@
 <?php
+require_once('./db.php');
+define('TESTING', TRUE);
 
 if (!empty($_POST))
 {
@@ -6,5 +8,19 @@ if (!empty($_POST))
 }
 else {
     echo "{ }";
+}
+?>
+
+<?php
+if (TESTING) {// Testing POST method handler
+?>
+<form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+    <input type="text" name="email" value="buyer5@example.com"><br>
+    <input type="text" name="password" value="55555"><br>
+    <input type="text" name="type" value="buyer"><br>
+    <input type="text" name="publickey" value="key55555key"><br>
+    <input type="submit" value="submit" name="Click">
+</form>
+<?php
 }
 ?>
